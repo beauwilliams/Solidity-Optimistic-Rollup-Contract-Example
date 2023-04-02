386882786108bce08b0c29c8d8c04a54ed9c01bf
beauwilliams <beau.williams@icloud.com>
2023-04-02 11:33:58 +1000

    feat: created a rollup layer 1 contract to store and verify hashes of proofs of transactions in efficient bytes 16 format

--- contracts/rollup.sol
+++ contracts/rollup.sol
@@ -1,22 +1,32 @@
+// SPDX-License-Identifier: BSD-4-Clause
+
 
 pragma solidity ^0.8.0;
 
 contract MerkleRootStorage {
-    bytes16 public merkleRootCompact;
-    mapping(bytes16 => bool) public verified;
-    bytes32 constant HASH_PREFIX = 0x1900;
-
-    function storeMerkleRoot(bytes32 _merkleRoot) public {
-        require(_merkleRoot != bytes32(0), "Merkle root cannot be zero");
-        bytes16 compact = bytes16(_merkleRoot);
-        require(!verified[compact], "Merkle root already verified");
-
-        merkleRootCompact = compact;
-        verified[compact] = true;
-        emit MerkleRootStored(compact);
+    constructor() {
+        sequencer = msg.sender;
     }
 
-    function verifyMerkleProof(bytes32[] memory leaves, bytes32[] memory proof) public view returns(bool[] memory) {
+    modifier onlySequencer() {
+        require(msg.sender == sequencer, "Only contract sequencer can call this function");
+        _;
+    }
+    address public sequencer;
+    bytes16 public merkleRootCompact;
+    mapping(bytes16 => bool) public verified;
+    bytes32 constant HASH_PREFIX = bytes32(uint256(6400));
+
+    function storeMerkleRoot(bytes16 _merkleRoot) public onlySequencer {
+        require(_merkleRoot != bytes16(0), "Merkle root cannot be zero");
+        require(!verified[_merkleRoot], "Merkle root already verified");
+
+        merkleRootCompact = _merkleRoot;
+        verified[_merkleRoot] = true;
+        emit MerkleRootStored(_merkleRoot);
+    }
+
+    function verifyMerkleProof(bytes16[] memory leaves, bytes16[] memory proof) public view returns(bool[] memory) {
         require(leaves.length == proof.length, "Arrays must be of the same length");
 
         bool[] memory results = new bool[](leaves.length);
@@ -26,6 +36,7 @@ contract MerkleRootStorage {
             hashed[i] = keccak256(abi.encodePacked(HASH_PREFIX, proof[i]));
         }
 
+        bytes16[] memory computedHashes = new bytes16[](leaves.length);
         for (uint256 i = 0; i < leaves.length; i++) {
             bytes32 computedHash = leaves[i];
 
@@ -37,7 +48,11 @@ contract MerkleRootStorage {
                 }
             }
 
-            bytes16 compact = bytes16(computedHash);
+            computedHashes[i] = bytes16(computedHash);
+        }
+
+        for (uint256 i = 0; i < computedHashes.length; i++) {
+            bytes16 compact = computedHashes[i];
             results[i] = verified[compact];
         }
 
--- test/test.ts
+++ test/test.ts
@@ -1,71 +1,65 @@
+// Import the necessary libraries
+const { expect } = require('chai');
+const { ethers } = require('hardhat');
 
-import { ethers } from 'hardhat';
-import { Contract, ContractFactory } from 'ethers';
-import { expect } from 'chai';
-
-describe('MerkleRootStorage', () => {
-  let contract: Contract;
-
-  const MOCK_MERKLE_ROOT =
-    '0x9c7e3df1cc35d7cc65c85b64de7c8e0d159712a470f18a551ae25e7cb382a276';
-  const MOCK_LEAVES = [
-    '0x47dfce4317f9e0e3c399cc12f1995a25138b0c5d9452dcf5f427e162e6d631c6',
-    '0xe1b157c1bde7dc58d16353d03cbf57c23811ca0f315e1f0bcde7c26642f75a2e',
-    '0xf3e8a69b745f95b5187c878a1d38aa7f06a9b2bb7ba47d282f3a8b97c28bc542',
-  ];
-  const MOCK_PROOFS = [
-    [
-      '0x7b996f76ccf78f6ed1f4a4a76458e44a4d8ceac5e6b5fc5e5f6bb05fc83843f3',
-      '0x197e3dadd3d9f5831760e9ef38d0a6b0478e04ec7ce57e294d6825f8bfe9f656',
-    ],
-    [
-      '0xd94e8f08b56e29fc98a76d7ff3d9f1b7a3150981d128b7a6e99a689ce42fbcf1',
-      '0x6ac9ed6f199dddb3d3f2f3ce8b57bc2741d583d66cd6c1f6a9afbdb2b2c67b4d',
-    ],
-    [
-      '0x4c4cbdd5f5e5d5e5c8b5f5c1cd5df523d9427c2e048452bbd7c2ed61bb3b7c1b',
-      '0x662a5cd1ab0087f0e99df352d87af6f8381e329a139e8ddfccaa3a2c3d3f7917',
-    ],
-  ];
-
-  before(async () => {
-    const contractFactory: ContractFactory = await ethers.getContractFactory(
-      'MerkleRootStorage'
-    );
-    contract = await contractFactory.deploy();
-    await contract.deployed();
-  });
-
-  it('should store a merkle root', async () => {
-    await contract.storeMerkleRoot(MOCK_MERKLE_ROOT);
-    expect(await contract.merkleRootCompact()).to.equal(
-      ethers.utils.hexDataSlice(MOCK_MERKLE_ROOT, 0, 16)
-    );
-  });
-
-  it('should not allow storing a zero merkle root', async () => {
-    await expect(contract.storeMerkleRoot('0x')).to.be.revertedWith(
-      'Merkle root cannot be zero'
-    );
-  });
-
-it('should verify a merkle proof', async () => {
-await contract.storeMerkleRoot(MOCK_MERKLE_ROOT);
-for (let i = 0; i < MOCK_LEAVES.length; i++) {
-const leaf = MOCK_LEAVES[i];
-const proof = MOCK_PROOFS[i];
-expect(await contract.verify(leaf, proof)).to.be.true;
-}
-});
-
-it('should reject an invalid merkle proof', async () => {
-await contract.storeMerkleRoot(MOCK_MERKLE_ROOT);
-const leaf = MOCK_LEAVES[0];
-// Change one of the nodes in the proof
-const invalidProof = [
-'0x7b996f76ccf78f6ed1f4a4a76458e44a4d8ceac5e6b5fc5e5f6bb05fc83843f3',
-'0x197e3dadd3d9f5831760e9ef38d0a6b0478e04ec7ce57e294d6825f8bfe9f655',
+// Define the mock data
+const MOCK_MERKLE_ROOT = "0x00000000000000000000000000000001";
+const MOCK_LEAVES = [
+    "0x00000000000000000000000000000002",
+    "0x00000000000000000000000000000003",
+    "0x00000000000000000000000000000004"
 ];
-expect(await contract.verify(leaf, invalidProof)).to.be.false;
-});
+const MOCK_PROOFS = [
+    "0x00000000000000000000000000000005",
+    "0x00000000000000000000000000000006",
+    "0x00000000000000000000000000000007"
+];
+
+describe("MerkleRootStorage contract", function() {
+  let merkleRootStorage: any;
+
+  beforeEach(async () => {
+    // Deploy the contract before each test
+    const MerkleRootStorage = await ethers.getContractFactory("MerkleRootStorage");
+    merkleRootStorage = await MerkleRootStorage.deploy();
+    await merkleRootStorage.deployed();
+  });
+
+  it("should store the merkle root correctly", async function() {
+    // Call the storeMerkleRoot function with the mock merkle root
+    await merkleRootStorage.storeMerkleRoot(MOCK_MERKLE_ROOT);
+
+    // Check that the merkle root was stored correctly
+    expect(await merkleRootStorage.merkleRootCompact()).to.equal(MOCK_MERKLE_ROOT);
+    expect(await merkleRootStorage.verified(MOCK_MERKLE_ROOT)).to.be.true;
+  });
+
+  it("should verify the merkle proof correctly", async function() {
+    // Call the storeMerkleRoot function with the mock merkle root
+    await merkleRootStorage.storeMerkleRoot(MOCK_MERKLE_ROOT);
+
+    // Call the verifyMerkleProof function with the mock leaves and proofs
+    const results = await merkleRootStorage.verifyMerkleProof(MOCK_LEAVES, MOCK_PROOFS);
+
+    // Check that the results match the expected values
+    expect(results).to.deep.equal([false, false, false]);
+  });
+
+  it("should not store a zero merkle root", async function() {
+    // Try to call the storeMerkleRoot function with a zero merkle root
+    await expect(merkleRootStorage.storeMerkleRoot(ethers.utils.hexZeroPad("0x",16))).to.be.revertedWith("Merkle root cannot be zero");
+  });
+
+  it("should not store a merkle root that has already been verified", async function() {
+    // Call the storeMerkleRoot function with the mock merkle root
+    await merkleRootStorage.storeMerkleRoot(MOCK_MERKLE_ROOT);
+
+    // Try to call the storeMerkleRoot function with the same merkle root again
+    await expect(merkleRootStorage.storeMerkleRoot(MOCK_MERKLE_ROOT)).to.be.revertedWith("Merkle root already verified");
+  });
+
+  it("should not verify a merkle proof with arrays of different lengths", async function() {
+    // Try to call the verifyMerkleProof function with arrays of different lengths
+    await expect(merkleRootStorage.verifyMerkleProof(MOCK_LEAVES.slice(0, 2), MOCK_PROOFS)).to.be.revertedWith("Arrays must be of the same length");
+  });
 });
